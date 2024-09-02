f78cec79e7788e2d4d473e7ce6755f2f698cc36d
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-09-02 10:35:27 +0200

    initial commit

--- Frame Of Mind/Frame_Of_MindApp.swift
+++ Frame Of Mind/Frame_Of_MindApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Frame_Of_MindApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Overview()
         }
     }
 }
---
+++ Frame Of Mind/Views/Overview.swift
@@ -0,0 +1,18 @@
+//
+//  Overview.swift
+//  Frame Of Mind
+//
+//  Created by Dustin Nuzzo on 02.09.24.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Overview()
+}
