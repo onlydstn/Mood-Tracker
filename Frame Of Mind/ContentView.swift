c564b926132ab8274bd2bca88834f6f4ea34506a
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-09-02 10:32:43 +0200

    Initial Commit

---
+++ Frame Of Mind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Frame Of Mind
+//
+//  Created by Dustin Nuzzo on 02.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Frame Of Mind/Frame_Of_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frame_Of_MindApp.swift
+//  Frame Of Mind
+//
+//  Created by Dustin Nuzzo on 02.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Frame_Of_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
